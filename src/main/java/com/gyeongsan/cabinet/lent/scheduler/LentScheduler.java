e4549f0cfc19837a9036832960a3e400f8d2f4d7
farmer0010 <zmqkf16@naver.com>
2026-02-09 09:49:34 +0900

    chore: change autoExtension scheduler time to 09:55 for testing

--- src/main/java/com/gyeongsan/cabinet/lent/scheduler/LentScheduler.java
+++ src/main/java/com/gyeongsan/cabinet/lent/scheduler/LentScheduler.java
@@ -37,7 +37,7 @@ public class LentScheduler {
     @Value("${cabinet.policy.lent-term}")
     private Integer lentTerm;
 
-    @Scheduled(cron = "0 40 1 * * *")
+    @Scheduled(cron = "0 55 9 * * *")
     @Transactional
     public void autoExtension() {
         log.info("🔔 [Daily] 자동 연장 프로세스 시작...");
